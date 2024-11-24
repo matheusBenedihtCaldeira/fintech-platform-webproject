89b103f596af1279712a83cbf5fb12f30d77049f
Matheus Benediht <matheusbenediht@gmail.com>
2024-11-23 23:01:57 -0300

    Update: Ajustando estrutura do projeto

--- fintech-platform-backend/fintech_platform_backend/app.py
+++ fintech-platform-backend/fintech_platform_backend/app.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
+# Instancia APP
 app = FastAPI()
 
-@app.get('/api')
-async def root():
-    return {'message': 'Hello, World!'}
+# Middlewares
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+api = APIRouter(prefix='/api')
+
+app.include_router(api)
